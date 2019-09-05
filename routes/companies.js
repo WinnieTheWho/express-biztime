091a7ac76f9b6462d95f8fd6f9438d76d57879f8
Kathy Nguyen <kathyn262@gmail.com>
2019-09-05 15:12:19 -0700

    working on delete

--- routes/companies.js
+++ routes/companies.js
@@ -1,67 +1,95 @@
 const express = require("express");
+const ExpressError = require("../expressError")
 const router = new express.Router();
 const db = require("../db")
 
 
-router.get('/', async function(req, res, next){
-    try {
-        const results = await db.query(`SELECT code, name FROM companies`)
-        return res.json({companies: results.rows});
-    } 
-    catch (err) {
-        return next(err);
-    }
+router.get('/', async function (req, res, next) {
+  try {
+    const results = await db.query(`SELECT code, name FROM companies`)
+    return res.json({ companies: results.rows });
+  }
+  catch (err) {
+    return next(err);
+  }
 })
 
-router.get('/:code', async function(req, res, next){
-    try {
-        const oneCode = req.params.code
-        
-        const results = await db.query(
-            `SELECT code, name, description
+router.get('/:code', async function (req, res, next) {
+  try {
+    const oneCode = req.params.code
+
+    const results = await db.query(
+      `SELECT code, name, description
              FROM companies 
              WHERE code=$1`, [oneCode])
-        return res.json({company: results.rows[0]})
-    } catch (err) {
-        return next(err);
+    
+    if (!results.rows[0]) {
+      throw new ExpressError("No such company!", 404);
     }
+
+    return res.json({ company: results.rows[0] })
+  } catch (err) {
+    return next(err);
+  }
 })
 
-router.post("/", async function(req, res, next){
-    try {
-        const { code, name, description } = req.body; 
+router.post("/", async function (req, res, next) {
+  try {
+    const { code, name, description } = req.body;
 
-        const result = await db.query(
-            `INSERT INTO companies (code, name, description)
+    const result = await db.query(
+      `INSERT INTO companies (code, name, description)
             VALUES ($1, $2, $3)
-            RETURNING code, name, description`, 
-            [code, name, description]
-        );
-        
-        return res.status(201).json({company: result.rows[0]});
-    } 
-    catch (err) {
-        return next(err)
-    }
+            RETURNING code, name, description`,
+      [code, name, description]
+    );
+
+    return res.status(201).json({ company: result.rows[0] });
+  }
+  catch (err) {
+    return next(err)
+  }
 })
 
-router.put("/:code", async function(req, res, next){
-    try {
-        const { code, name, description } = req.body; 
+router.put("/:code", async function (req, res, next) {
+  try {
+    const { code, name, description } = req.body;
 
-        const result = await db.query(
-            `UPDATE companies 
+    const result = await db.query(
+      `UPDATE companies 
             SET name=$1, description=$2
             WHERE code=$3
-            RETURNING code, name, description`, 
-            [name, description,req.params.code]
-        );
-        
-        return res.json({company: result.rows[0]});
-    } 
-    catch (err) {
-        return next(err)
+            RETURNING code, name, description`,
+      [name, description, req.params.code]
+    );
+
+    if (!result.rows[0]) {
+      throw new ExpressError("No such company!", 404);
     }
+
+    return res.json({ company: result.rows[0] });
+  }
+  catch (err) {
+    return next(err)
+  }
+})
+
+router.delete("/:code", async function (req, res, next) {
+  try {
+    const result = await db.query(
+      `DELETE FROM companies WHERE code=$1`,
+      [req.params.code]
+    );
+
+    if (!result.rows[0]) {
+      throw new ExpressError("No such company!", 404);
+    }
+
+    return res.json({status: "deleted"});
+  }
+  catch (err) {
+    return next(err)
+  }
 })
 
 module.exports = router; 
---
+++ routes/invoices.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const ExpressError = require("../expressError")
+const router = new express.Router();
+const db = require("../db")
+
+router.get("/", async function (req, res, next) {
+  try {
+    const results = await db.query(`SELECT id, comp_code FROM invoices`)
+    return res.json({ invoices: results.rows });
+  }
+  catch (err) {
+    return next(err);
+  }
+})
+
+router.get("/:id", async function(req, res, next) {
+  try {
+    // include comp_code to use it later on in companyResult
+    // can delete it from the returned result later on, so don't worry about it 
+    
+    const invoiceResult = await db.query(`SELECT id, comp_code, amt, paid, 
+                                         add_date, paid_date FROM invoices WHERE id=$1`,
+                                         [req.params.id])
+
+    // invoiceResult still has empty row even with invalid id 
+    if (!invoiceResult.rows[0]) {
+      throw new ExpressError("No such invoice!", 404);
+    }
+
+    const companyResult = await db.query(`SELECT code, name, description 
+                                         FROM companies WHERE code=$1`,
+                                         [invoiceResult.rows[0].comp_code])
+
+    // need const result, can't dump into return statement bc adding company to it
+    const result = invoiceResult.rows[0]
+
+
+    // add company object into the returned result
+    result.company = companyResult.rows[0]
+    // remove comp_code in returned result
+    delete result.comp_code;
+    
+    return res.json({ invoice: result });
+  }
+  catch (err) {
+    return next(err);
+  }
+})
+
+router.post("/", async function(req, res, next) {
+  try {
+    const { comp_code, amt } = req.body;
+
+    // the rest of the values have default values preset in data.sql 
+    // so that's why we're able to return them without errors
+    
+    const result = await db.query(
+      `INSERT INTO invoices (comp_code, amt)
+            VALUES ($1, $2)
+            RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+      [comp_code, amt]
+    );
+
+    return res.status(201).json({ invoice: result.rows[0] });
+  }
+  catch (err) {
+    return next(err)
+  }
+}
+)
+
+router.put("/:id", async function(req, res, next) {
+  try {
+    const { amt } = req.body;
+
+    const result = await db.query(
+      `UPDATE invoices
+       SET amt=$1
+       WHERE id=$2
+       RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+       [amt, req.params.id]
+    );
+
+    if (!result.rows[0]) {
+      throw new ExpressError("No such invoice!", 404);
+    }
+
+    return res.json({ invoice: result.rows[0] });
+  }
+  catch (err) {
+    // we have two error messages bc of line 26-28 in app.js
+    // they want to return 2 error messages
+    return next(err)
+  }
+})
+
+router.delete("/:id", async function(req, res, next) {
+  
+})
+
+module.exports = router; 
